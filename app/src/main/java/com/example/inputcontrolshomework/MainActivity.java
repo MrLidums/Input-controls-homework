2c548c389b9ecf75f2116cae6b84f9eb1b1ec13f
MrLidums <arturs.lidums@gmail.com>
2019-10-24 18:29:21 +0300

    checkboxes and button created

--- app/src/main/java/com/example/inputcontrolshomework/MainActivity.java
+++ app/src/main/java/com/example/inputcontrolshomework/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.inputcontrolshomework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String cb1, cb2, cb3, cb4, cb5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showSelectedCheckboxes(View view) {
+
+    }
+
+    public void storeText(View view) {
+        CheckBox checkBox = (CheckBox) findViewById(view.getId());
+        switch (view.getId()){
+
+        }
     }
 }
